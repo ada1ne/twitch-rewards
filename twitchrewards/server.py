991b9c7f888079e70aaaf83c64453d70acded730
Ada <ada.dvora@gmail.com>
2025-01-17 10:31:30 -0300

    Change default port to 443

--- twitchrewards/server.py
+++ twitchrewards/server.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "twitchrewards.main:app",
         host="0.0.0.0",
-        port=8000,
+        port=443,
         reload=True,
         ssl_keyfile=settings.SSL_KEY_PATH,
         ssl_certfile=settings.SSL_CERTIFICATE_PATH,
