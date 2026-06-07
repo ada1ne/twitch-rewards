9f58931495c0c7c7fb3c1a92d0c1c8494ddbb428
Ada <ada.dvora@gmail.com>
2026-06-07 10:58:41 -0300

    Prevent default and use spread operator

--- twitchrewards/static/home/trophies-form.js
+++ twitchrewards/static/home/trophies-form.js
@@ -1,5 +1,6 @@
 function handleSubmitActiveTrophies(e) {
-    const checked_trophies = document.querySelectorAll('.active-trophy-option:checked');
+    e.preventDefault();
+    const checked_trophies = [...document.querySelectorAll('.active-trophy-option:checked')];
     const checked_trophies_ids = checked_trophies.map((trophy_element) => trophy_element.getAttribute("data-trophy-id"));
 
     alert(`Checked trophies are ${JSON.stringify(checked_trophies_ids)}`);
