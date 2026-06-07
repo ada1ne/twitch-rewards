9899ed52ebec30fc9fca5c40e27b400e8f2f0e49
Ada <ada.dvora@gmail.com>
2026-06-07 11:23:57 -0300

    Convert to JSON before submit

--- twitchrewards/static/home/trophies-form-fo.js
+++ twitchrewards/static/home/trophies-form-fo.js
@@ -3,7 +3,10 @@ function handleSubmitActiveTrophies(e) {
     const checked_trophies = [...document.querySelectorAll('.active-trophy-option:checked')];
     const checked_trophies_ids = checked_trophies.map((trophy_element) => parseInt(trophy_element.getAttribute("data-trophy-id")));
 
-    fetch("/users/active-trophies", { method: "POST", body: {'trophies_ids': JSON.stringify(checked_trophies_ids)}, headers: { "Content-Type": "application/json" } })
+    const data = {
+        'trophies_ids': JSON.stringify(checked_trophies_ids)
+    }
+    fetch("/users/active-trophies", { method: "POST", body: JSON.stringify(data), headers: { "Content-Type": "application/json" } })
         .then((response) => {
             if (!response.ok) {
                 alert('Algo deu errado. Avisa no Discord!');
