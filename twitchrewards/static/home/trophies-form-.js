03773a1516599b0d209ea6810f76b828398709f8
Ada <ada.dvora@gmail.com>
2026-06-07 11:17:19 -0300

    Submit active trophies

--- twitchrewards/static/home/trophies-form-.js
+++
@@ -1,9 +0,0 @@
-function handleSubmitActiveTrophies(e) {
-    e.preventDefault();
-    const checked_trophies = [...document.querySelectorAll('.active-trophy-option:checked')];
-    const checked_trophies_ids = checked_trophies.map((trophy_element) => trophy_element.getAttribute("data-trophy-id"));
-
-    alert(`Checked trophies are ${JSON.stringify(checked_trophies_ids)}`);
-}
-
-document.getElementById('submit-active-trophies').addEventListener("click", handleSubmitActiveTrophies);
---
+++ twitchrewards/static/home/trophies-form-foo.js
@@ -0,0 +1,17 @@
+function handleSubmitActiveTrophies(e) {
+    e.preventDefault();
+    const checked_trophies = [...document.querySelectorAll('.active-trophy-option:checked')];
+    const checked_trophies_ids = checked_trophies.map((trophy_element) => parseInt(trophy_element.getAttribute("data-trophy-id")));
+
+    fetch("/users/active-trophies", { method: "POST", body: {'trophies_ids': JSON.stringify(checked_trophies_ids)}, headers: { "Content-Type": "application/json" } })
+        .then((response) => {
+            if (!response.ok) {
+                alert('Algo deu errado. Avisa no Discord!');
+            }
+            else {
+                alert('Foi! Se você mudou durante a live, espera 5 minutinhos pra aparecer la');
+            }
+        });
+}
+
+document.getElementById('submit-active-trophies').addEventListener("click", handleSubmitActiveTrophies);
